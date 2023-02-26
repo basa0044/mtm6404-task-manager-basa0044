2f0449ff7d16ea4462f76dd1cb72b1294694791e
Feyza Basaran <basa0044@algonquinlive.com>
2023-02-26 17:51:12 -0500

    Completes the assignment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Header = (props) => {
     return(
-        <div>
+        <div className="header">
+            <i>icon</i>
             <p>Welcome, Friend!</p>
         </div>
     )
@@ -22,46 +23,93 @@ const Navbar = (props) => {
 
 const Title = (props) => {
     return(
-        <div>
-        <p>Title of the Page goes Here</p>
+        <div className="title">
+        <p>TODAY</p>
         <button><i>Icon</i>Add a To Do</button>
-        <ul>
-            <li>Complete</li>
-            <li>Item</li>
-            <li>Priority</li>
-            <li>Time</li>
-            <li>Delete</li>
-        </ul>
+        <div>
+            <div className="complete"><p>Complete</p></div>
+            <div className="item"><p>Item</p></div>
+            <div className="titles">
+                <p>Priority</p>
+                <p>Time</p>
+                <p>Delete</p>
+            </div>
+        </div>
         </div>
     )
 }
 
 const ListItems = (props) => {
     return(
-        <div>
-            <span>icon</span>
-            <p>Meeting with the Team</p>
-            <span>High</span>
-            <p>10.30</p>
-            <i>Icon</i>
+        <div className="listItems">
+        <div className="list">
+            <input type="checkbox"></input>
+            <p className="textarea">Meeting with the Team</p>
+            <div className="variants">
+                <span><p>High</p></span>
+                <p className="time">10.30</p>
+                <i>Icon</i>
+            </div>
+        </div>
+        <div className="list">
+            <input type="checkbox"></input>
+            <p className="textarea">Project Due</p>
+            <div className="variants">
+                <span><p>High</p></span>
+                <p className="time">11.59</p>
+                <i>Icon</i>
+            </div>
+        </div>
+        <div className="list">
+            <input type="checkbox"></input>
+            <p className="textarea">Class</p>
+            <div className="variants">
+                <span><p>Medium</p></span>
+                <p className="time">17.00</p>
+                <i>Icon</i>
+            </div>
+        </div>
+        <div className="list">
+            <input type="checkbox"></input>
+            <p className="textarea">Workout</p>
+            <div className="variants">
+                <span><p>Low</p></span>
+                <p className="time">21.30</p>
+                <i>Icon</i>
+            </div>
+        </div>
+        <div className="list">
+            <input type="checkbox"></input>
+            <p className="textarea">Movie time with sister</p>
+            <div className="variants">
+                <span><p>Low</p></span>
+                <p className="time">22.30</p>
+                <i>Icon</i>
+            </div>
+        </div>
         </div>
     )
 } 
 
 const Footer = (props) => {
     return(
-        <div><p>© 2023 Feyza Basaran. All rights reserved.</p></div>
+        <footer><p>© 2023 Feyza Basaran. All rights reserved.</p></footer>
+    )
+}
+
+function App (){
+    return(
+        <React.Fragment>
+        <Header/>
+        <Navbar/>
+        <Title/>
+        <ListItems/>
+        <Footer/>
+        </React.Fragment>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(
-    <React.Fragment>
-    <Header/>
-    <Navbar/>
-    <Title/>
-    <ListItems/>
-    <Footer/>
-    </React.Fragment>
-)
+root.render(<App/>)
+
