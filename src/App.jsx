be0d8f5659fabee3d10d33ac4c50a8e4c32f6d47
Feyza Basaran <basa0044@algonquinlive.com>
2023-03-16 17:18:50 -0400

    some style changes on list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM  from 'react-dom/client'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Title from './components/Title'
@@ -10,22 +11,34 @@ import './style.css'
 
 let items = [
   {
-      "index": 0,
-      "task": "Cooking", 
-      "priority": "High",
-      "time": 10.30,
+    "index": 0,
+    "task": "Clean your room", 
+    "priority": "Low",
+    "time": '18.30',
   },
   { 
-      "index": 1,
-      "task": "Cooking", 
-      "priority": "High",
-      "time": 10.30,
+    "index": 1,
+    "task": "Workout", 
+    "priority": "Medium",
+    "time": '21.30',
   },
   {
-      "index": 2,
-      "task": "Cooking", 
-      "priority": "High",
-      "time": 10.30,
+    "index": 2,
+    "task": "Movie Time", 
+    "priority": "Medium",
+    "time": '22.30',
+  },
+  {
+    "index": 3,
+    "task": "Cooking", 
+    "priority": "High",
+    "time": '17.00',
+  },
+  {
+    "index": 4,
+    "task": "Lecture", 
+    "priority": "High",
+    "time": '8.00',
   }
   ]
 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,17 +5,21 @@
 
 export default  function List (props) {
     const items = props.items
+    
     if(items.length){
-    const ListItems = items.map( item => 
-    <div className="list">
-    <input type="checkbox"></input>
-    <span className="textarea">{item.task}</span>
-    <div className="variants">
-        <span>{item.priority}</span>
-        <p className="time">{item.time}</p>
-        <i>Icon</i>
-    </div>
-   </div>)
+    
+        const ListItems = items.map( item => 
+        <div className="list">
+            <input type="checkbox"></input>
+            <span className="textarea">{item.task}</span>
+            <div className="variants">
+                <span>{item.priority}</span>
+                <span className="color"></span>
+                <p className="time">{item.time}</p>
+                <i>Icon</i>
+            </div>
+        </div>
+    )
     return(
       <ul className='listItems'>{ListItems}</ul>
       ) }else{
