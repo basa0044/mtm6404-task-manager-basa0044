8d9ac26f385b2a25137acb3c484ffc828c1e6d2f
Feyza Basaran <basa0044@algonquinlive.com>
2023-03-17 13:00:22 -0400

    branch check

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -29,41 +29,5 @@ export default  function List (props) {
         }          
   }
     
-        // {/* <div className="list">
-        //     <input type="checkbox"></input>
-        //     <p className="textarea">Project Due</p>
-        //     <div className="variants">
-        //         <span><p>High</p></span>
-        //         <p className="time">11.59</p>
-        //         <i>Icon</i>
-        //     </div>
-        // </div>
-        // <div className="list">
-        //     <input type="checkbox"></input>
-        //     <p className="textarea">Class</p>
-        //     <div className="variants">
-        //         <span><p>Medium</p></span>
-        //         <p className="time">17.00</p>
-        //         <i>Icon</i>
-        //     </div>
-        // </div>
-        // <div className="list">
-        //     <input type="checkbox"></input>
-        //     <p className="textarea">Workout</p>
-        //     <div className="variants">
-        //         <span><p>Low</p></span>
-        //         <p className="time">21.30</p>
-        //         <i>Icon</i>
-        //     </div>
-        // </div>
-        // <div className="list">
-        //     <input type="checkbox"></input>
-        //     <p className="textarea">Movie time with sister</p>
-        //     <div className="variants">
-        //         <span><p>Low</p></span>
-        //         <p className="time">22.30</p>
-        //         <i>Icon</i>
-        //     </div>
-        // </div> */}
-        // </div>
+
     
