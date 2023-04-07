18af43bd206c51816ce1b6d7705c9cdfdd54fdb3
Feyza Basaran <basa0044@algonquinlive.com>
2023-04-07 14:16:26 -0400

    react router setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar(){
     return(
         <nav>
             <span>TASK MANAGER</span>
             <ul>
-                <li>House Chores</li>
-                <li>Today</li>
-                <li>Grocery List</li>
+                <li>
+                <Link to="/">Today</Link>
+                </li>
+                <li>
+                <Link to="/New">New Page</Link>
+                </li>
             </ul>
             <button><i className="fa-solid fa-plus"></i>Add a Page</button>
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Header from './components/Header'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:<App/>, 
+  },
+  {
+    path: "/new",
+    element:<Navbar/>
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
